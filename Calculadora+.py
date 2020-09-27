0f6faa8637582c2ef67da0506e01b9f0c488b862
vermelhachan <71857226+vermelhachan@users.noreply.github.com>
2020-09-27 20:54:57 -0300

    Update and rename README.md to Calculadora+.py

---
+++ Calculadora+.py
@@ -0,0 +1,5 @@
+# android
+print("Coloque os numeros")
+nm1=int(input("Coloque"))
+nm2=int(input("Coloque"))
+print(nm1 + nm2)
--- README.md
+++
@@ -1,2 +0,0 @@
-# android
-Criei esse código em Python para criar um Sistema útil do android. Com várias ferramentas.
